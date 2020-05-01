100edaf2c28157fadfac63cf005f1044b72a3726
Brian Shumate <brian@brianshumate.com>
2020-05-01 18:13:44 -0400

    Trying to get env var set

--- vault-init/courseBase.sh
+++ vault-init/courseBase.sh
@@ -33,8 +33,8 @@ cat << EOF >> "$MICROVAULT_VAULT_CONFIG"
   }
 EOF
 
-export PATH=/home/scrapbook/tutorial/.bin:$PATH
+export PATH="/home/scrapbook/tutorial/.bin:$PATH"
 
-printf "\n\nexport VAULT_ADDR=http://127.0.0.1:8200\n" >> /root/.profile
+printf "\n\nexport VAULT_ADDR=http://127.0.0.1:8200\n" >> /root/.bashrc
 
 nohup sh -c "/root/.bin/vault server -config /root/vault/config > /root/vault/log/vault.log 2>&1" > /root/vault/log/nohup.log &
