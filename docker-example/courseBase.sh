09372f870700375dd85217193174527f662823af
Brian Shumate <brian@hashicorp.com>
2020-06-22 11:35:53 -0400

    Updates

--- docker-example/courseBase.sh
+++ docker-example/courseBase.sh
@@ -3,9 +3,9 @@
 
 terraform_version="0.12.26"
 
-# Get Terraform
+# Download and install Terraform
 curl -L -o "$HOME"/terraform.zip https://releases.hashicorp.com/terraform/"$terraform_version"/terraform_"$terraform_version"_linux_amd64.zip
 unzip -d  /usr/local/bin/ "$HOME"/terraform.zip
 
-# Get repository
+# Clone repository
 git clone https://github.com/brianshumate/vss.git
--- docker-example/terminal.sh
+++ docker-example/terminal.sh
@@ -1,2 +1,2 @@
-# echo 'export VAULT_ADDR="http://127.0.0.1:8200"' >> /etc/bash.bashrc
-# echo 'export PATH="$PATH:$HOME/.bin"' >> /etc/bash.bashrc
+#!/bin/sh
+cd vss || printf 2> "oh noes"
