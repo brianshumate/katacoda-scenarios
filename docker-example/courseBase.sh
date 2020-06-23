31494666b5524adbc0aa57bd70256f7e431171c4
Brian Shumate <brian@hashicorp.com>
2020-06-23 09:35:59 -0400

    Why

--- docker-example/courseBase.sh
+++ docker-example/courseBase.sh
@@ -4,8 +4,8 @@
 export terraform_version="0.12.26"
 
 # Download and install Terraform
-curl -L -o "$HOME"/terraform.zip https://releases.hashicorp.com/terraform/"$terraform_version"/terraform_"$terraform_version"_linux_amd64.zip
-unzip -d  /usr/local/bin/ "$HOME"/terraform.zip
+curl -L -o "$HOME"/terraform.zip https://releases.hashicorp.com/terraform/"$terraform_version"/terraform_"$terraform_version"_linux_amd64.zip && \
+unzip -d  /usr/local/bin/ "$HOME"/terraform.zip && \
 
 # Clone repository
 git clone https://github.com/brianshumate/vss.git
