ad358bd8ad016b405d4c2196a815c4ce79148e7c
Brian Shumate <brian@hashicorp.com>
2020-06-24 12:34:52 -0400

    Log verify failures

--- vault-telemetry-splunk/02-initialize-and-unseal-vault.md
+++ vault-telemetry-splunk/02-initialize-and-unseal-vault.md
@@ -2,7 +2,7 @@
 
 The default initialization without arguments results in Vault using the [Shamir's Secret Sharing algorithm](https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing) to split the unseal key in to 5 key shares with a required quorum of 3 unseal keys needed to successfully unseal Vault.
 
-For this example, you can use just one key share to speed up the manual unseal process.
+For this example, you can use just 1 key share to speed up the manual unseal process.
 
 > **NOTE**: You might have noticed that when this step began, a `VAULT_ADDR` environment variable was exported to a URL that represents the Vault docker container. This is done to ensure your commands address the correct server.
 
--- vault-telemetry-splunk/02-initialize-and-unseal-vault.md.verify.sh
+++ vault-telemetry-splunk/02-initialize-and-unseal-vault.md.verify.sh
@@ -5,7 +5,7 @@ if docker ps -f name=vtl-vault --format "{{.Status}}" | grep -qw healthy
   then
     echo "Vault server is healthy"
   else
-    >&2 echo "Vault server container is not healthy. Please ensure it is healthy, and that you have initialized and unsealed it before continuing."
+    echo "Vault server container is not healthy. Please ensure it is healthy, and that you have initialized and unsealed it before continuing."  >> 02-init-and-unseal.log 2>&1
 fi
 
 
@@ -13,7 +13,7 @@ if vault status -format=json | jq -r '.initialized' | grep -q true
   then
     echo "Vault server is initialized"
   else
-    >&2 echo "Vault server is not initialized. Please initialize it, unseal it, and login before continuing."
+    echo "Vault server is not initialized. Please initialize it, unseal it, and login before continuing."  >> 02-init-and-unseal.log 2>&1
 fi
 
 
@@ -21,5 +21,5 @@ if vault token lookup -format=json | jq -r '.data.policies[0]' | grep -q root
   then
     echo "Vault token contains root policy"
   else
-    >&2 echo "Vault token does not contain root policy; you need to login with the initial root token before continuing."
+    echo "Vault token does not contain root policy; you need to login with the initial root token before continuing." >> 02-init-and-unseal.log 2>&1
 fi
