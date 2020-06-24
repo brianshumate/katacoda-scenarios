dac50ca5b6b51bcbaaff763fa501845947bcacfd
Brian Shumate <brian@hashicorp.com>
2020-06-24 12:43:14 -0400

    log step 1

--- vault-telemetry-splunk/01-start-containers.md.verify.sh
+++ vault-telemetry-splunk/01-start-containers.md.verify.sh
@@ -4,5 +4,5 @@ if [[ $(docker ps -f name=vtl-splunk --format "{{.Status}}" | grep -w healthy) ]
 then
   echo "done"
 else
-  >&2 echo "Splunk container is not healthy. Please ensure it is up and healthy before continuing."
+  echo "Splunk container is not healthy. Please ensure it is up and healthy before continuing." >> 01-start-containers.log 2>&1
 fi
