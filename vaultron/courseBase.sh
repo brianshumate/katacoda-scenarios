9ef210270d9801447bbced03dd1eb95b9d78f087
Brian Shumate <brian@hashicorp.com>
2020-07-06 10:56:08 -0400

    hello

--- vaultron/courseBase.sh
+++ vaultron/courseBase.sh
@@ -8,13 +8,15 @@ export vault_version="1.4.2"
 
 mkdir -p "$log_dir"
 
+echo "HELLO" >> "$log_dir"/install.log
+
 # ensure unzip and uuid-runtime are installed
 apt update && \
-apt install -y unzip uuid-runtime >> "$log_dir"/install.log 2>&1
+apt install -y unzip uuid-runtime >> "$log_dir"/install.log
 
 # Download function
 download() {
-  curl --fail --location --silent --show-error --output "$HOME"/"${1}".zip https://releases.hashicorp.com/"${1}"/"${2}"/"${1}"_"${2}"_linux_amd64.zip  >> "$log_dir"/install.log 2>&1
+  curl --fail --location --silent --show-error --output "$HOME"/"${1}".zip https://releases.hashicorp.com/"${1}"/"${2}"/"${1}"_"${2}"_linux_amd64.zip  >> "$log_dir"/install.log
 }
 
 # Install function
@@ -24,11 +26,11 @@ install() {
   rm -f "$HOME"/"$1".zip
 }
 
-download terraform "$terraform_version" >> "$log_dir"/install.log 2>&1 && \
-install terraform >> "$log_dir"/install.log 2>&1 && \
-download vault "$vault_version" >> "$log_dir"/install.log 2>&1 && \
-install vault >> "$log_dir"/install.log 2>&1 && \
-download consul "$consul_version" >> "$log_dir"/install.log 2>&1 && \
-install consul >> "$log_dir"/install.log 2>&1
+download terraform "$terraform_version" >> "$log_dir"/install.log && \
+install terraform >> "$log_dir"/install.log && \
+download vault "$vault_version" >> "$log_dir"/install.log && \
+install vault >> "$log_dir"/install.log && \
+download consul "$consul_version" >> "$log_dir"/install.log && \
+install consul >> "$log_dir"/install.log
 
-git clone --branch v3.4.4 https://github.com/brianshumate/vaultron.git >> "$log_dir"/install.log 2>&1
+git clone --branch v3.4.4 https://github.com/brianshumate/vaultron.git >> "$log_dir"/install.log
